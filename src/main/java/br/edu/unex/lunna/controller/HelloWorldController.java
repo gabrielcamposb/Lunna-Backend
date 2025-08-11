e52f109bf3d3392922a753c0ec2cb1fd5da68a53
Gabriel <gabrielcampossb@gmail.com>
2025-08-11 20:00:01 -0300

    Add files via upload

---
+++ src/main/java/br/edu/unex/lunna/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package br.edu.unex.lunna.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
